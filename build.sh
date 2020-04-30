83840b2b1db161cd3ef363dbbfc7a3f6e2d4a822
Kostas Evangelou <kevangel@admin.grnet.gr>
2020-04-30 12:54:48 +0300

    Check if version exists in registry before build

--- build.sh
+++ build.sh
@@ -15,7 +15,11 @@ for d in */ ; do
     VERSION=`cat $d/VERSION`
     IMAGE=`echo ${d%/}`
     echo '>>> Image:' $IMAGE:$VERSION
-
+    version_exists=`curl $REGISTRY/v2/$IMAGE/tags/list | grep "${VERSION}" ; exit 0`
+    if [[ "$version_exists" ==  *"$VERSION"* ]]; then
+        echo ">>> Skiping build. Image version already exists in registry"
+        continue;
+    fi
     # Build Docker image and tag version
     docker build -f $IMAGE/Dockerfile . -t $IMAGE:latest
     docker tag $IMAGE:latest $IMAGE:$VERSION
